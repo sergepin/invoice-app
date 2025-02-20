2dd6cbc86612c963f079fca61345a2a1df37a473
Sergio Pinzon <sergiopin596@gmail.com>
2025-02-19 22:03:25 -0500

    app working

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'invoice-app' title`, () => {
+  it(`should have the 'invoice-fe' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('invoice-app');
+    expect(app.title).toEqual('invoice-fe');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, invoice-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, invoice-fe');
   });
 });
